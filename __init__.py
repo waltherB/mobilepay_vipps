2d466af59348d05a83da76a1d80260655e8c1ade
Walther Barnett <walther.barnett@gmail.com>
2025-08-28 14:23:01 +0200

    Define module-level hook wrappers delegating to hooks.py to satisfy Odoo hook invocation

--- __init__.py
+++ __init__.py
@@ -2,4 +2,16 @@
 
 from . import models
 from . import controllers
-from .hooks import pre_init_check, post_init_hook, uninstall_hook
+from . import hooks as _hooks
+
+
+def pre_init_check(*args, **kwargs):
+    return _hooks.pre_init_check(*args, **kwargs)
+
+
+def post_init_hook(*args, **kwargs):
+    return _hooks.post_init_hook(*args, **kwargs)
+
+
+def uninstall_hook(*args, **kwargs):
+    return _hooks.uninstall_hook(*args, **kwargs)
