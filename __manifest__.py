c444436a1eb0b1baa17c39b081403c6d006ff5ce
Walther Barnett <walther.barnett@gmail.com>
2025-09-08 15:36:52 +0200

    Fix: Add temporary pos_payment_method_views.xml to resolve another cached dependency
    - Created minimal pos_payment_method_views.xml file to satisfy Odoo's cached reference
    - Added file reference to manifest.py data section
    - This resolves another FileNotFoundError during module installation
    - Both temporary view files can be removed once Odoo cache is cleared

--- __manifest__.py
+++ __manifest__.py
@@ -93,6 +93,7 @@ Website: https://github.com/waltherB/mobilepay_vipps
         # Views
         'views/payment_provider_views.xml',
         'views/payment_transaction_views.xml',
+        'views/pos_payment_method_views.xml',
     ],
     'assets': {
         'web.assets_frontend': [
