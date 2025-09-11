063a3f82e85345e237120510c0ee7b1d6feb7faf
Walther Barnett <walther.barnett@gmail.com>
2025-09-11 11:42:01 +0200

    indent error

--- models/payment_provider.py
+++ models/payment_provider.py
@@ -427,9 +427,9 @@ class PaymentProvider(models.Model):
             
             response = requests.post(token_url, headers=headers, timeout=30)
             if response.status_code != 200:
-                error_msg = f"Vipps API fejl i {token_url}. Status: {response.status_code}, Besked: {response.text}"
-               _logger.error(error_msg)
-               raise ValidationError(_(error_msg))
+		    	error_msg = f"Vipps API fejl i {token_url}. Status: {response.status_code}, Besked: {response.text}"
+			    _logger.error(error_msg)
+			    raise ValidationError(_(error_msg))
             
             response.raise_for_status()
             
