86644f499fd366fe9dced0cd142e24c72cbc71a8
Walther Barnett <walther.barnett@gmail.com>
2025-09-12 12:12:14 +0200

    Load error fixed

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
+from . import vipps_profile_wizard
 from . import payment_provider
 from . import payment_transaction
 from . import res_partner
@@ -12,7 +13,6 @@ from . import res_partner
 #     logging.getLogger(__name__).info("POS module not available - skipping POS models")
 from . import vipps_api_client
 from . import vipps_data_management
-from . import vipps_profile_wizard
 from . import vipps_onboarding_wizard
 from . import vipps_security
 from . import vipps_webhook_security
